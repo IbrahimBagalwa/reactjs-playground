1733db3aafbcd9916cc09f54ed0ca20802d60ae3
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-03-23 20:57:12 +0200

    feat:function component

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,46 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import { CardList } from "./components/card-list";
 import { SearchBox } from "./components/search-box";
 
-class App extends Component {
-  // the constructor runs before any thing, basicaly in side your constructor you should initialize the state
-  // after the constructor to run the render run next, the render will determine what to show
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: "",
+const App = () => {
+  const url = "https://jsonplaceholder.typicode.com/users";
+  // useState Hook gives us the ability to encuptilate local state in the function components.
+
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+
+  useEffect(() => {
+    let ignore = false;
+    async function fetchingUsers() {
+      const res = await fetch(url);
+      const data = await res.json();
+      if (!ignore) {
+        setMonsters(data);
+      }
+    }
+
+    fetchingUsers();
+
+    return () => {
+      ignore = true;
     };
-  }
+  }, [url]);
 
-  // lofecycle of a component.
-  // componentDidMount() is a method that use have access to, and it runs whenever the component run for the first time.\
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) =>
-        this.setState(() => {
-          return { monsters: data };
-        })
-      );
-  }
-
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLowerCase();
-    this.setState(() => {
-      return { searchField };
-    });
+  const onSearchChange = (e) => {
+    let searchField = e.target.value.toLowerCase();
+    setSearchField(searchField);
   };
-  render() {
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-    const filterMonsters = monsters.filter((monster) =>
-      monster.name.toLowerCase().includes(searchField)
-    );
-    return (
-      <div>
-        <h1 className="app-title">Monsters Rolodex</h1>
-        <SearchBox onChangeHandler={onSearchChange} />
-        <CardList monsters={filterMonsters} />
-      </div>
-    );
-  }
-}
+  const filterMonsters = monsters.filter((monsters) =>
+    monsters.name.toLowerCase().includes(searchField)
+  );
+
+  return (
+    <div>
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox onChangeHandler={onSearchChange} />
+      <CardList monsters={filterMonsters} />
+    </div>
+  );
+};
 
 export default App;
