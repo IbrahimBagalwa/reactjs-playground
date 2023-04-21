d841df6956a3b9309a70237e4b68e5155830314b
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-04-21 11:53:47 +0200

    feat: experimnent useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+
+const BadFunction = () => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("Bad stopping watch");
+    setInterval(() => {
+      setCount((v) => v + 0.1);
+    }, 1000);
+  }, []);
+  return <div>BadStop watch: {count.toFixed(1)}</div>;
+};
+
+const GoodFunction = () => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("Good stopping watch");
+    const interval = setInterval(() => {
+      setCount((v) => v + 0.1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  return <div>Good stop watch: {count.toFixed(1)}</div>;
+};
 function App() {
   return (
-    <div className="text-blue-400 text-center">
-      <p>React Playground Projects</p>
+    <div className="grid grid-cols-2">
+      <GoodFunction />
+      <BadFunction />
     </div>
   );
 }
