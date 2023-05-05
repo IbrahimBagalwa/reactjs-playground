deb2cfee9ea83c1d351f33d2e6f93a65869d917b
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-05-05 11:34:29 +0200

    feat: build our serverless function

--- src/components/payment-form/PaymentForm.jsx
+++ src/components/payment-form/PaymentForm.jsx
@@ -1,13 +1,25 @@
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import { Button } from "../ui";
 import { BUTTON_TYPE_CLASSES } from "../ui/Button/Button";
+import { FormContainer, PaymentFormContainer } from "./payment-fom.style";
 
 const PaymentForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+    if (!stripe && !elements) return;
+  };
+
   return (
-    <div>
-      <CardElement />
-      <Button buttonTypes={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
-    </div>
+    <PaymentFormContainer>
+      <FormContainer>
+        <h2>Credit Card Payment :</h2>
+        <CardElement />
+        <Button buttonTypes={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
+      </FormContainer>
+    </PaymentFormContainer>
   );
 };
 
