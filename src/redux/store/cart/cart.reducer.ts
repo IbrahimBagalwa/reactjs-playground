8a12a6ae41807fc5e8d404291381afdf82ff1cda
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-05-07 16:10:23 +0200

    feat:migrate cart reducr

---
+++ src/redux/store/cart/cart.reducer.ts
@@ -0,0 +1,25 @@
+import { setCartItems, setIsCartOpen } from "./cart.action";
+import { CartItem } from "./cart.types";
+import { AnyAction } from "redux";
+
+export type CartState = {
+  isCartOpen: boolean;
+  cartItems: CartItem[];
+};
+const DEFAULT_STATE: CartState = {
+  isCartOpen: false,
+  cartItems: [],
+};
+export const cartReducer = (
+  state = DEFAULT_STATE,
+  action: AnyAction
+): CartState => {
+  if (setIsCartOpen.match(action)) {
+    return { ...state, isCartOpen: action.payload };
+  }
+  if (setCartItems.match(action)) {
+    return { ...state, cartItems: action.payload };
+  }
+
+  return state;
+};
