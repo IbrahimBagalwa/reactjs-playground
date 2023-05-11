ecb5e45dad328258a66f4feaaef793f149fae28e
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-05-11 12:24:49 +0200

    feat: implement graphql in the categories context

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,24 +1,40 @@
-import { createContext, useState, useEffect } from 'react';
-
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import { createContext, useEffect, useState } from "react";
+import { gql, useQuery } from "@apollo/client";
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
 export const CategoriesProvider = ({ children }) => {
+  const { loading, error, data } = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
-
+  console.log(data);
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
-
-    getCategoriesMap();
-  }, []);
-
-  const value = { categoriesMap };
+    if (data) {
+      const { collections } = data;
+      const collecctionsMap = collections.reduce((acc, collection) => {
+        const { title, items } = collection;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      }, {});
+      setCategoriesMap(collecctionsMap);
+    }
+  }, [data]);
+  const value = { categoriesMap, loading, error };
   return (
     <CategoriesContext.Provider value={value}>
       {children}
