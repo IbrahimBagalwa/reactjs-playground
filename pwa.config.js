8a4bf60c06cea19f5214d1f19ebbdcf279419778
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-05-24 17:26:15 +0200

    feat: updating our code to be pwa compliant

---
+++ pwa.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  name: "Crwn Clothing", // The name of your app
+  short_name: "CrwnClothing", // The short name of your app
+  theme_color: "#fff", // The theme color of your app
+  background_color: "#fff", // The background color of your app
+  display: "standalone", // The display mode of your app
+  scope: "/", // The scope of your app
+  start_url: "/", // The start URL of your app
+  icons: [
+    {
+      src: "./favicon.ico", // The path to your app icon
+      sizes: [96, 128, 192, 256, 384, 512], // The sizes of your app icon
+    },
+    {
+      src: "./crwn-512x512.png",
+      sizes: [512],
+      type: "image/png",
+    },
+    {
+      src: "./crwn-16x16.png",
+      sizes: [192],
+      type: "image/png",
+    },
+  ],
+};
